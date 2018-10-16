09cdd4306bfaae3b1853739d6475521c85902e8e
Sezer Bölük <white.code.text@gmail.com>
2018-10-16 16:30:45 +0300

    post

--- index.php
+++ index.php
@@ -1,69 +1,13 @@
 <?php 
-    class download  {
-        
-        protected function clearUrl($url){
-            if(isset($url)) {
-                if(!empty($url)) {
-                   return htmlspecialchars($url);
-                }
-            }
-        }
 
-        protected function isUrl($url){
-            if(isset($url)) {
-                $url = $this->clearUrl($url);
-                return $url;
-            }
-        }
+$data   = array('name' => 'john' , 'age' => 31);
 
-        protected function returnExtension($url){
-            $extension = @end(preg_split('/[.+?]/' , $url));
-            return $extension;
-            
-        }
+$string = http_build_query($data);
 
-        public function downloadFile($url){
-            if(isset($url)) { 
-                if(!empty($url)) {
-                $extension = $this->returnExtension($url);
-                $url       = $this->isUrl($url);
-    
-                // CRUL 
-                $cr = curl_init();
-                curl_setopt($cr  ,  CURLOPT_URL , $url); // işlem yapılacak url 
-                curl_setopt($cr  ,  CURLOPT_RETURNTRANSFER , true);
-                curl_setopt($cr  ,  CURLOPT_SSL_VERIFYPEER , true);
-                $curl = curl_exec($cr);
-                curl_close($cr); 
+$ch = curl_init('http://localhost/crul/data.php');
+curl_setopt($ch , CURLOPT_POST , true );
+curl_setopt($ch , CURLOPT_POSTFIELDS , $string) ;
+curl_setopt($ch , CURLOPT_RETURNTRANSFER , true);
 
-                $id = md5(uniqid());
-                $file = fopen("download/$id.".$extension , 'w+');
-                if(fputs($file , $curl)) {
-                    echo 'dosya eklendi';
-                }
- 
-                
-            }
-         
-
-        }
-
-        }
-
-    }
-
-      $obj = new download();
-    
-    ?>
-        <form method="POST">
-            <input type="text" name="search">
-            <input type="submit" name="btn" value="downloadFile">
-        </form>
-
-        <?php
-
-            if($_POST) {
-                $obj->downloadFile($_POST['search']);
-            }
-        
-        ?>
+$response = curl_exec($ch);
+curl_close($ch);
